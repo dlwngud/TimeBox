d36a2ca50b0d19c637cb06cb8e83aa9f744b8364
이주형 <leewngud980411@gmail.com>
2026-01-07 23:38:50 +0900

    [feat] BrainDumpItem 매핑 확장함수

--- app/src/main/java/com/wngud/timebox/data/local/BrainDumpEntity.kt
+++ app/src/main/java/com/wngud/timebox/data/local/BrainDumpEntity.kt
@@ -2,7 +2,10 @@ package com.wngud.timebox.data.local
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.wngud.timebox.presentation.brainDump.BrainDumpItem
+import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Locale
 
 @Entity(tableName = "brain_dump_items")
 data class BrainDumpEntity(
@@ -11,3 +14,12 @@ data class BrainDumpEntity(
     val content: String,
     val timestamp: Date = Date()
 )
+
+fun BrainDumpEntity.toBrainDumpItem(): BrainDumpItem {
+    val formatter = SimpleDateFormat("yyyy.MM.dd HH:mm", Locale.getDefault())
+    return BrainDumpItem(
+        id = this.id,
+        content = this.content,
+        formattedTimestamp = formatter.format(this.timestamp)
+    )
+}
